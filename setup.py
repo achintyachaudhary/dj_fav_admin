a06a33c4c3cd76d1b1558c874993b7ccc10f5ac5
aR chaudhary <achintya.ranjan@hashedin.com>
2020-08-06 20:25:50 +0530

    removed egg files

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
-#!/usr/bin/env python
+import os
+from setuptools import setup
 
-if __name__ == "__main__":
-    import setuptools
-    setuptools.setup()
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+setup(
+    name='trollism1234',
+    version='0.1',
+    packages=['favorite'],
+    description='A line of description',
+    long_description=README,
+    author='yourname',
+    author_email='yourname@example.com',
+    url='https://github.com/achintyachaudhary/dj_fav_admin/',
+    license='MIT',
+    install_requires=[
+        'chaudhary',
+    ]
+)
